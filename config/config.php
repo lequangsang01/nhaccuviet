0adbdcd1a215f129f5b6ebba0d427ee4b124ecd7
Sang Le <quangsangle.hn@gmail.com>
2021-10-11 21:12:27 +0700

    up4

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
-define("DB_HOST", "mysql5027.site4now.net");
-define("DB_USER", "a7af20_nhaccuv");
-define("DB_PASS", "SangLe10032001");
-define("DB_NAME", "db_a7af20_nhaccuv");
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "bai_thi");
 
 ?>
 
